99ab5dbbc6f455d1e1918562c4fabfb61a28d17f
daisuzz <daisaku.rugby@gmail.com>
2025-03-22 20:07:35 +0900

    first commit

---
+++ app.py
@@ -0,0 +1,65 @@
+import json
+import logging
+import os
+import sys
+
+from linebot import LineBotApi, WebhookHandler
+from linebot.exceptions import InvalidSignatureError, LineBotApiError
+from linebot.models import MessageEvent, TextMessage, TextSendMessage
+
+# INFOレベル以上のログメッセージを拾うように設定する
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
+# 環境変数からMessaging APIのチャネルアクセストークンとチャネルシークレットを取得する
+CHANNEL_ACCESS_TOKEN = os.environ["CHANNEL_ACCESS_TOKEN"]
+CHANNEL_SECRET = os.environ["CHANNEL_SECRET"]
+
+# それぞれ環境変数に登録されていないとエラー
+if CHANNEL_ACCESS_TOKEN is None:
+    logger.error(
+        "Specify CHANNEL_ACCESS_TOKEN as environment variable.")
+    sys.exit(1)
+if CHANNEL_SECRET is None:
+    logger.error("Specify CHANNEL_SECRET as environment variable.")
+    sys.exit(1)
+
+line_bot_api = LineBotApi(CHANNEL_ACCESS_TOKEN)
+webhook_handler = WebhookHandler(CHANNEL_SECRET)
+
+@webhook_handler.add(MessageEvent, message=TextMessage)
+def handle_message(event):
+
+    # 応答トークンを使って回答を応答メッセージで送る
+    line_bot_api.reply_message(
+        event.reply_token,
+        TextSendMessage(text=event.message.text))
+
+def lambda_handler(event, context):
+
+    # リクエストヘッダーにx-line-signatureがあることを確認
+    if 'x-line-signature' in event['headers']:
+        signature = event["headers"]["x-line-signature"]
+
+    body = event["body"]
+    # 受け取ったWebhookのJSONを目視確認できるようにINFOでログに吐く
+    logger.info(body)
+
+    try:
+        webhook_handler.handle(body, signature)
+    except InvalidSignatureError:
+        # 署名を検証した結果、飛んできたのがLINEプラットフォームからのWebhookでなければ400を返す
+        return {
+            "statusCode": 400,
+            "body": json.dumps('Only webhooks from the LINE Platform will accepted.')
+        }
+    except LineBotApiError as e:
+        # 応答メッセージを送ろうとしたがLINEプラットフォームからエラーがかえってきたらエラーを吐く
+        logger.error("Got exception from LINE Messaging API: %s\n" % e.message)
+        for m in e.error.details:
+            logger.error("  %s: %s" % (m.property, m.message))
+
+    return {
+        "statusCode": 200,
+        "body": json.dumps('Hello from Lambda!')
+    }
